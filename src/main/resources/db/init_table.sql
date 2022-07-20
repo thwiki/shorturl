8158eaf376a9b0b79a7f2e751dd5ee6805303372
htdong <dht31261@gmail.com>
2022-07-20 21:43:49 +0800

    c

--- src/main/resources/db/init_table.sql
+++ src/main/resources/db/init_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS short_url
 (
    id BIGINT (20) NOT NULL AUTO_INCREMENT,
    gmt_create datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   gmt_modified datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+   gmt_modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    short_path varchar (16) NOT NULL,
    real_url varchar (2048) NOT NULL,
    PRIMARY KEY (id),
