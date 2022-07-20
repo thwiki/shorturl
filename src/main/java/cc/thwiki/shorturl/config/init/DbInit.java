8d5a4f843509a3a0ad5a3ddae867b739994c7d63
htdong <dht31261@gmail.com>
2022-07-20 21:20:58 +0800

    c

---
+++ src/main/java/cc/thwiki/shorturl/config/init/DbInit.java
@@ -0,0 +1,50 @@
+package cc.thwiki.shorturl.config.init;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DbInit {
+
+    @Resource
+    private DataSource druidDataSource;
+
+    @PostConstruct
+    public void init() throws SQLException, IOException {
+        try (Connection connection = druidDataSource.getConnection()) {
+            try (Statement stat = connection.createStatement()) {
+                executeSql(stat, new ClassPathResource("init_table.sql"));
+            }
+        }
+    }
+
+    private void executeSql(Statement stat, ClassPathResource sqlFile) throws IOException, SQLException {
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(sqlFile.getInputStream()))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        }
+        String[] s = sb.toString().split(";\n");
+        for (String i : s) {
+            if (StringUtils.isNotBlank(i)) {
+                stat.addBatch(i);
+            }
+        }
+        stat.executeBatch();
+        stat.clearBatch();
+    }
+}
---
+++ src/main/resources/db/init_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS short_url
+(
+   id BIGINT (20) NOT NULL AUTO_INCREMENT,
+   gmt_create datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   gmt_modified datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+   short_path varchar (16) NOT NULL,
+   real_url varchar (2048) NOT NULL,
+   PRIMARY KEY (id),
+   UNIQUE KEY (short_path)
+)
+ENGINE= InnoDB DEFAULT CHARSET= utf8_bin;
