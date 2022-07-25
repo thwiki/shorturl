3fb0345ebe7a03b81b3190845c537a940628f4cf
htdong <dht31261@gmail.com>
2022-07-25 15:45:31 +0800

    c

--- src/main/resources/db/init_table.sql
+++ src/main/resources/db/init_table.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS short_url
    gmt_modified datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    short_path varchar (16) NOT NULL,
    real_url varchar (2048) NOT NULL,
+   expire_time BIGINT (20) DEFAULT NULL,
    PRIMARY KEY (id),
-   UNIQUE KEY (short_path)
+   UNIQUE KEY 'uk_shortpath' (short_path),
+   KEY 'idx_expiretime' (expire_time)
 )
 ENGINE= InnoDB DEFAULT CHARSET = utf8 COLLATE=utf8_bin;
