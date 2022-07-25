9077c33cfb8af4b0fdbfa46d7c8b4c4cd65c1b42
htdong <dht31261@gmail.com>
2022-07-25 15:36:47 +0800

    c

--- src/main/java/cc/thwiki/shorturl/service/impl/ShortUrlServiceImpl.java
+++ src/main/java/cc/thwiki/shorturl/service/impl/ShortUrlServiceImpl.java
@@ -40,12 +40,14 @@ public class ShortUrlServiceImpl implements ShortUrlService {
     }
 
     private static String randomShortPath() {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        int len = random.nextInt(3) + 5;
+        long t = System.currentTimeMillis() / 10;
+        String s1 = (t / 10000) + "";
+        String s2 = (t % 10000) + "";
+        t = Long.parseLong(s2 + s1);
         StringBuilder sb = new StringBuilder();
-        while (len > 0) {
-            --len;
-            sb.append(BASE62[random.nextInt(BASE62.length)]);
+        while (t != 0) {
+            sb.append(BASE62[(int)(t % BASE62.length)]);
+            t /= BASE62.length;
         }
         return sb.toString();
     }
